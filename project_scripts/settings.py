5e902f8d0a7033e0113f992d8c7c313a4c78829f
MustyMuffin <85773042+MustyMuffin@users.noreply.github.com>
2024-10-15 18:26:14 -0700

    Added shooting mechanics

--- project_scripts/settings.py
+++ project_scripts/settings.py
@@ -8,5 +8,11 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (200, 230, 230)
 
+        # Bullet Settings
+        self.bullet_speed = 12.0
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60, 60, 60)
+
         # Ship settings
-        self.ship_speed = 1.5
+        self.ship_speed = 4
